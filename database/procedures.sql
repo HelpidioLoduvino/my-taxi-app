5f90f1b92a766bc3969358049440d189e5283102
Onesimo Martins <Ogouveia86@gmail.com>
2023-07-02 00:50:34 +0100

    feat: funcao finalizar_viagem

--- database/procedures.sql
+++ database/procedures.sql
@@ -43,16 +43,41 @@ BEGIN
     insert into viagem(preco, id_pedido, id_motorista, data_inicio, data_fim)
     values (null, id_pedido, id_motorista, current_timestamp, null);
 
-    update motorista p set estado= 'OCUPADO' where id=id_motorista;
+    update motorista p set estado= 'OCUPADO' where id = id_motorista;
+
+END;
+$$ LANGUAGE plpgsql;
+
+drop function finalizar_viagem(pedido_id int);
+
+create or replace procedure finalizar_viagem(pedido_id int)
+     AS
+$$
+BEGIN
+
+    update pedido set estado='FINALIZADO' where id=pedido_id;
+    update viagem p set data_fim= current_timestamp where id_pedido=pedido_id;
+
+    update motorista p set estado= 'DISPONIVEL' where id = (
+        select v.id_motorista from viagem v where v.id_pedido=pedido_id
+        );
+
+    update utilizador u  set localizacao= (select p.destino from pedido p where p.id=pedido_id)
+                         where u.id = (select u.id from utilizador u2
+                                        inner join motorista m2 on u2.id = m2.id_utilizador
+                                        inner join viagem v on m2.id = v.id_motorista
+                                        where v.id_pedido=id_pedido);
+
 
 END;
 $$ LANGUAGE plpgsql;
 
 
+call iniciar_viagem(2, 1);
+select *from pedido;
 
+select *from viagem;
 
+select *from motorista;
 
-call iniciar_viagem(2,1);
-select  * from pedido;
-select * from viagem;
-select * from motorista;
+call finalizar_viagem(1);
